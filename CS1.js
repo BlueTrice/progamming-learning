4c439f5bfcbe289ccbf5e2e69dd8f5c62e8ce32b
blue <bluetrice2@gmail.com>
2023-08-03 15:12:33 +0700

    Upload ke 2 kalinya

---
+++ CS1.js
@@ -0,0 +1,27 @@
+// Game fantasy Proxytia
+// Masukan nama anda
+// Tentukan karakter anda
+// Akan ada peringatan apabila nama atau role kosong
+// Ketauhi apa yang karakter anda bisa lakukan
+
+let name = `Blue`;
+let role = `knight`;
+
+if (name == ``) {
+    console.log(`Please insert your name!`);
+}
+
+console.log(name);
+
+if (role == `knight`) {
+    console.log(`halo Ksatria ${name},kamu dapat menyerang dengan senjatamu!`);
+}    else if (role == `mage`) {
+    console.log(`halo Penyihir ${name},ciptakan keajaiban yang membantu kemenanganmu! `);
+} else if (role == `healer`) {
+    console.log(`halo Tabib ${name},kamu akan membantu temanmu yang terluka!`);
+} else if (role == ``) {
+    console.log(`Pilih peranmu untuk memulai game!`);
+} else {console.log(`tapi kayaknya kamu jadi bot aja ya, peran yang kamu pilih ga ada`);
+}
+
+console.log(role);
---
+++ CS2.js
@@ -0,0 +1,47 @@
+//Memasukan hari bulan dan tahun dengan angka
+//Mengubah bulan(angka) menjadi nama bulan dengan conditional switch case
+
+let hari = '1';
+let bulan = 5;
+let tahun = '1945';
+
+switch(bulan) {
+    case 1:
+        bulan = "Januari";
+        break;
+    case 2:
+        bulan = "Februari";
+        break;
+    case 3:
+        bulan = "Maret";
+    case 4:
+        bulan = "April";
+        break;
+    case 5:
+        bulan = "Mei";
+        break;
+    case 6:
+        bulan = "Juni";
+        break;
+    case 7:
+        bulan = "Juli";
+        break;
+    case 8:
+        bulan = "Agustus";
+        break;
+    case 9:
+        bulan = "September";
+        break;
+    case 10:
+        bulan = "Oktober";
+        break;
+    case 11:
+        bulan = "November";
+        break;
+    case 12:
+        bulan = "Desember";
+        break;
+}
+waktu = hari + " " + bulan + " " + tahun;
+
+console.log(waktu);
