8f8f74f538e6a0e0d4b591b538c0a54e1f20521d
blue <bluetrice2@gmail.com>
2023-08-09 19:52:01 +0700

    Upload ke 5

---
+++ quizLooping.js
@@ -0,0 +1,72 @@
+// Quiz 1
+
+let i = 0;
+
+console.log('LOOPING PERTAMA WHILE: ');
+while (i <= 5)  {
+    console.log(i);
+    i++
+}
+
+console.log('LOOPING KEDUA WHILE: ');
+i = 5;
+while (i >= 0) {
+    console.log(i);
+    i--
+}
+
+// Quiz 2
+
+console.log('LOOPING PERTAMA FOR: ');
+for (i = 0; i <= 5; i++) {
+    console.log(i);
+}
+
+console.log('LOOPING KEDUA FOR: ')
+for (i = 5; i >= 0; i--) {
+    console.log(i);
+}
+
+//Quiz 3
+
+for (i = 0; i <= 100; i++) {
+    if (i % 2 === 0) {
+        console.log( `${i} adalah genap`);
+    } else {
+        console.log( `${i} adalah ganjil`);
+    }
+}
+
+//Quiz 4
+
+for (i = 1; i <= 100; i += 2) {
+        if (i % 3 === 0) {
+            console.log(`${i} 3 kelipatan 3`);
+        } else {
+            console.log(i);
+        }
+}
+
+for (i = 1; i <= 100; i += 5) {
+    if (i % 6 === 0) {
+        console.log(`${i} 3 kelipatan 3`);
+    } else {
+        console.log(i);
+    }
+}
+
+for (i = 1; i <= 100; i += 9) {
+    if (i % 10 === 0) {
+        console.log(`${i} 3 kelipatan 3`);
+    } else {
+        console.log(i);
+    }
+}
+
+//Quiz 5
+
+let stars = ["*", "**", "***", "****", "*****"];
+
+for (i = 0; i <= 4; i++) {
+    console.log(stars[i]);
+}
